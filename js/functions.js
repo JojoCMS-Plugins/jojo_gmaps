6b5b962e91b6f0b2ebc3df7f74ba307c83c45776
Tom Dale <tom@zero.co.nz>
2015-02-25 01:09:06 +0000

    gMaps: Complete rewrite to allow more than one map per page. Externalocation links are currently broken but I think everything else works

--- js/functions.js
+++ js/functions.js
@@ -1,6 +1,18 @@
+var apikey = "";
+var sensor = "false";
+var marker = "";
+var infowindow = "";
+
 $(document).ready(function() {
     if ($('.jojo_map').length>0) {
-        loadGMapScript();
+        var lang = $('html').attr('lang');
+        var apikey = $('.jojo_map').attr('data-apikey');
+        var sensor = $('.jojo_map').attr('data-sensor');
+        var script = document.createElement("script");
+        script.type = "text/javascript";
+        script.src = "http://maps.googleapis.com/maps/api/js?" + ( apikey ? "key=" + apikey : "" ) + "&sensor=" + sensor + "&callback=initializeMap&language=" + lang;
+        document.body.appendChild(script);
+
     }
 });
 
@@ -31,3 +43,92 @@ function findonmap(map, markerid) {
  * Build: http://modernizr.com/download/#-geolocation
  */
 ;window.Modernizr=function(a,b,c){function t(a){i.cssText=a}function u(a,b){return t(prefixes.join(a+";")+(b||""))}function v(a,b){return typeof a===b}function w(a,b){return!!~(""+a).indexOf(b)}function x(a,b,d){for(var e in a){var f=b[a[e]];if(f!==c)return d===!1?a[e]:v(f,"function")?f.bind(d||b):f}return!1}var d="2.6.2",e={},f=b.documentElement,g="modernizr",h=b.createElement(g),i=h.style,j,k={}.toString,l={},m={},n={},o=[],p=o.slice,q,r={}.hasOwnProperty,s;!v(r,"undefined")&&!v(r.call,"undefined")?s=function(a,b){return r.call(a,b)}:s=function(a,b){return b in a&&v(a.constructor.prototype[b],"undefined")},Function.prototype.bind||(Function.prototype.bind=function(b){var c=this;if(typeof c!="function")throw new TypeError;var d=p.call(arguments,1),e=function(){if(this instanceof e){var a=function(){};a.prototype=c.prototype;var f=new a,g=c.apply(f,d.concat(p.call(arguments)));return Object(g)===g?g:f}return c.apply(b,d.concat(p.call(arguments)))};return e}),l.geolocation=function(){return"geolocation"in navigator};for(var y in l)s(l,y)&&(q=y.toLowerCase(),e[q]=l[y](),o.push((e[q]?"":"no-")+q));return e.addTest=function(a,b){if(typeof a=="object")for(var d in a)s(a,d)&&e.addTest(d,a[d]);else{a=a.toLowerCase();if(e[a]!==c)return e;b=typeof b=="function"?b():b,typeof enableClasses!="undefined"&&enableClasses&&(f.className+=" "+(b?"":"no-")+a),e[a]=b}return e},t(""),h=j=null,e._version=d,e}(this,this.document);
+
+
+function initializeMap() {
+    var mapid = '';
+    $('.jojo_map').each( function(){
+        mapid = $(this).attr('id');
+        sensor = $(this).attr('data-sensor') ? true : false;
+        locations = $('#maplocations' + mapid + ' > div');
+       /* Set center to New Zealand if no locations are provided */
+       if (locations) {
+            latlng = locations.first().attr('data-latlng').split(',');
+            middle = new google.maps.LatLng(latlng[0],latlng[1]);
+        } else {
+            middle = new google.maps.LatLng(-40.900557,174.885971);
+        }
+        if ($(this).attr('data-maptype')=='Satellite') {
+                mapType = google.maps.MapTypeId.SATELLITE;
+        } else if ($(this).attr('data-maptype')=='Hybrid') {
+                mapType = google.maps.MapTypeId.HYBRID;
+        } else if ($(this).attr('data-maptype')=='Terrain') {
+                mapType = google.maps.MapTypeId.TERRAIN;
+        } else {
+                mapType = google.maps.MapTypeId.ROADMAP;
+        }
+        mapOptions = { 
+            center : middle,
+            zoom : ( $(this).attr('data-zoom') && $(this).attr('data-zoom')!='auto' ? parseInt($(this).attr('data-zoom')) : 5 ),
+            scaleControl : ( $(this).attr('data-scale') ? true : false ),
+            draggable: ( $(this).attr('data-draggable') ? true : false ),
+            scrollwheel: ( $(this).attr('data-scroll') ? true : false ),
+            mapTypeId : mapType,
+            navigationControl: ( $(this).attr('data-nav') ? true : false ),
+            navigationControlOptions: {
+                style: ( $(this).attr('data-navstyle') ? google.maps.NavigationControlStyle.SMALL : google.maps.NavigationControlStyle.DEFAULT )
+            },
+            streetViewControl: ( $(this).attr('data-streetview') ? true : false ),
+            mapTypeControl: ( $(this).attr('data-typecontrol') ? true : false ),
+            mapTypeControlOptions: { style: google.maps.MapTypeControlStyle.DROPDOWN_MENU }       
+        };
+        gmap = new google.maps.Map(document.getElementById(mapid), mapOptions);
+        bounds = new google.maps.LatLngBounds();
+        if (locations) {
+            locations.each( function(k) {
+                locationid = $(this).attr('id');
+                geoloc = $(this).attr('data-latlng').split(',');
+                ltitle = $('h3', this).html();
+                ldesc = $('p', this).html();
+                pos = new google.maps.LatLng(geoloc[0],geoloc[1]);
+                marker = new google.maps.Marker({
+                    position: pos,
+                    map: gmap,
+                    title: ltitle
+                });
+                bounds.extend(new google.maps.LatLng(geoloc[0],geoloc[1]));
+                infowindow = new google.maps.InfoWindow({
+                        content: ''
+                });
+                description = '<p class="mapinfo"><strong>' + ltitle +'</strong><br />'+ ldesc + '</p>';
+                bindInfoWindow(marker, gmap, infowindow, description);
+
+            });
+           if ($(this).attr('data-zoom')=='auto') {
+                gmap.fitBounds(bounds);
+            }
+        }
+        if (sensor == true && Modernizr.geolocation) {
+            navigator.geolocation.getCurrentPosition(function(position){
+                var myLocation = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+                var me = new google.maps.Marker({
+                    position: myLocation,
+                    icon: '/images/my-location.png',
+                    anchor : new google.maps.Point(8,8),
+                    map: gmap,
+                    title: 'You are here!',
+                    zIndex: 100
+                });
+                bounds.extend(myLocation);
+                gmap.fitBounds(bounds);
+            });
+        }
+     });
+}
+
+function bindInfoWindow(marker, map, infowindow, strDescription) {
+    google.maps.event.addListener(marker, 'click', function() {
+        infowindow.setContent(strDescription);
+        infowindow.open(map, marker);
+    });
+}
